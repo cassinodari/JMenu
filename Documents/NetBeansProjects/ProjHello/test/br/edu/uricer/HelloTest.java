1674b1bf5d828099e346e7149e74080f03bc0f3c
cassinodari <cassi.nodari@hotmail.com>
2016-08-18 20:02:13 -0300

    Criado os métodos

---
+++ Documents/NetBeansProjects/ProjHello/test/br/edu/uricer/HelloTest.java
@@ -0,0 +1,26 @@
+package br.edu.uricer;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public HelloTest() {
+    
+    @Test
+        public void testRetornarHelloNomeSePassarNome() {
+        Hello hello = new Hello("Cassiane");
+        assertEquals("Hello Cassiane", hello.getMessage ();
+    }
+
+@Test
+        public void testRetornarHelloSemNome() {
+         Hello hello = new Hello;
+          assertEquals("Hello", hello.getMessage ();
+    }
+        
+    }
+    
+}
